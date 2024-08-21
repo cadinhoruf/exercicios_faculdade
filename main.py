ec09ebb99ba7e16092f8ff47f1b9439232bbe5ec
cadinhorufjr <cadinhorufjr@icloud.com>
2024-08-20 21:30:09 -0300

    feat: added comments

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 print('Bem vindo a Loja do Ricardo Rufino')
+
+# Capturando o valor do pedido
 valorDoPedido = float(input("Entre com o valor do pedido: "))
+
+# Capturando o valor da quantidade de parcelas
 quantidaDeParcelas = int(input("Entre com a quantidade de parcelas: "))
+
+# Declaração da variável juros
 juros =0
+
+# Lógica para o cálculo de juros, reatribuindo o valor para cada condição
 if quantidaDeParcelas < 4:
   juros = 0
 elif quantidaDeParcelas >= 4 and quantidaDeParcelas < 6:
@@ -13,7 +21,11 @@ elif quantidaDeParcelas >= 9 and quantidaDeParcelas < 13:
 else:
   juros = 32/100
 
+# Valor total da parcela baseado nos juros
 valorDaParcela = valorDoPedido * (1 + juros) / quantidaDeParcelas 
-valorTotalParcelado = valorDaParcela  * quantidaDeParcelas
+
+# Valor total parcelado 
+valorTotalParcelado = float(valorDaParcela  * quantidaDeParcelas)
+
 print(f'O valor das parcelas é de R$: {valorDaParcela:.2f}')
-print(f'O valor total parcelado é de R$: {valorTotalParcelado}')
+print(f'O valor total parcelado é de R$: {valorTotalParcelado:.2f}')
